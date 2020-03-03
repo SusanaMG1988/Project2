d9b427ca849bc2ba1ae8299db0ea8bf3842d9954
susifu <susanamg1988@hotmail.com>
2020-03-03 18:56:43 +0100

    Cambio de nombre a la tabla pasteles a recetas-de-pasteles

--- database/migrations/2020_03_03_175453_create_recetas-de-pasteles_table.php
+++ database/migrations/2020_03_03_175453_create_recetas-de-pasteles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePastelesTable extends Migration
+class CreateRecetasDePastelesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePastelesTable extends Migration
      */
     public function up()
     {
-        Schema::create('pasteles', function (Blueprint $table) {
+        Schema::create('recetas-de-pasteles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreatePastelesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pasteles');
+        Schema::dropIfExists('recetas-de-pasteles');
     }
 }
